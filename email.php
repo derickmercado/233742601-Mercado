6d32598c31e7aa16d82d0d9ec6cbb5145f46cc21
Derick Myles Mercado <derick.mercado2124@gmail.comm>
2024-10-23 14:44:25 +0800

    updated folder

---
+++ email.php
@@ -0,0 +1,21 @@
+<?php
+
+include("index.html");
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+
+    $to = "derick.mercado2124@gmail.com";
+    $subject = "New Survey Submission";
+    $message = "Name: $name\nEmail: $email";
+    $headers = "From: $email";
+
+    if (mail($to, $subject, $message, $headers)) {
+        echo "Thank you! Your survey has been submitted.";
+    } else {
+        echo "Sorry, something went wrong. Please try again.";
+    }
+}
+?>
