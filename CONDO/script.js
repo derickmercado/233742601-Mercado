0a75c3998a480edd1a9c92d44d57bb2015466b86
Jeanel Kaye Wakat <kaidelighted@gmail.com>
2025-02-13 16:02:01 +0800

    updated activity

--- CONDO/script.js
+++ CONDO/script.js
@@ -64,3 +64,11 @@ document.addEventListener("DOMContentLoaded", function () {
     window.addEventListener("scroll", updateActiveSection);
     updateActiveSection(); 
 });
+
+const burger = document.getElementById('burger');
+const navLinks = document.getElementById('nav-links');
+
+burger.addEventListener('click', () => {
+    navLinks.classList.toggle('nav-active');
+    burger.classList.toggle('toggle');
+});
